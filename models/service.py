6d43dfe6691bdaf23248b3525dfa42cb17778181
Bhavya Malhotra <242802181+code-1py@users.noreply.github.com>
2026-06-27 10:53:29 +0530

    feat : service models to store nmap extracted data for services on a port

---
+++ models/service.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass , field
+
+@dataclass
+class Service:
+    name:str|None = None
+    product:str|None = None
+    servicefp:str|None = None
+    version:str|None = None
+    method:str|None = None
+    tunnel:str|None = None
+    conf:str|None = None
